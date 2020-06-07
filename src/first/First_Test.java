5895602e1e06476cbe2aca09cec5eeb77a56b0d8
junia92 <66466511+junia92@users.noreply.github.com>
2020-06-07 14:23:34 +0900

    printf

--- src/first/First_Test.java
+++ src/first/First_Test.java
@@ -4,6 +4,9 @@ public class First_Test {
 	public static void main(String[] agrs) {
 		int a = 1;
 		int b = 100;
-		System.out.printf("%d + %d : %d",a,b,  a+b);
+		System.out.printf("%d + %d : %d \n",a,b,  a+b);
+		
+		String test = "DaeHwan";
+		System.out.printf("%s", test);
 	}
 }
