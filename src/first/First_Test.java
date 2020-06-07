4a60e7a11ed58d503642c677e57e5a39cde08787
junia92 <66466511+junia92@users.noreply.github.com>
2020-06-07 14:18:28 +0900

    Hello-test

--- src/first/First_Test.java
+++ src/first/First_Test.java
@@ -4,6 +4,6 @@ public class First_Test {
 	public static void main(String[] agrs) {
 		int a = 1;
 		int b = 100;
-		System.out.printf("SUM : %d + %d", a+b);
+		System.out.printf("%d + %d : %d",a,b,  a+b);
 	}
 }
